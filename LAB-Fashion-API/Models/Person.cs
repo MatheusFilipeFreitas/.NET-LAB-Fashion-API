47000fdfa673830ba779da0f9b3c190becd57cad
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-06-17 23:31:27 -0300

    Model de Pessoa

---
+++ LAB-Fashion-API/Models/Person.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LAB_Fashion_API.Models
+{
+    public class Person
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotNull]
+        public string Name { get; set; } = string.Empty;
+        [NotNull]
+        public string Sex { get; set; } = string.Empty;
+
+        [Column(TypeName = "Date")]
+        public DateTime Birthday { get; set; }
+        [NotNull]
+        public string Cpf { get; set; } = string.Empty;
+        [NotNull]
+        public string Cnpj { get; set; } = string.Empty;
+        [NotNull]
+        public string Phone { get; set; } = string.Empty;
+    }
+}
