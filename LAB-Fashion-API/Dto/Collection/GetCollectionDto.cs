af539cb70206437f9aee8f4c41d15743d73d6e3a
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-06-19 19:48:24 -0300

    DTO de Colecao

---
+++ LAB-Fashion-API/Dto/Collection/GetCollectionDto.cs
@@ -0,0 +1,16 @@
+﻿using LAB_Fashion_API.Enums;
+
+namespace LAB_Fashion_API.Dto.Collection
+{
+    public class GetCollectionDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int AccountableId { get; set; }
+        public string Brand { get; set; }
+        public double Budget { get; set; }
+        public DateTime Release { get; set; }
+        public Seasons Season { get; set; }
+        public StatusType Status { get; set; }
+    }
+}
---
+++ LAB-Fashion-API/Dto/Collection/UpdateCollectionDto.cs
@@ -0,0 +1,14 @@
+﻿using LAB_Fashion_API.Enums;
+
+namespace LAB_Fashion_API.Dto.Collection
+{
+    public class UpdateCollectionDto
+    {
+        public string Name { get; set; }
+        public int AccountableId { get; set; }
+        public string Brand { get; set; }
+        public double Budget { get; set; }
+        public DateTime Release { get; set; }
+        public Seasons Season { get; set; }
+    }
+}
