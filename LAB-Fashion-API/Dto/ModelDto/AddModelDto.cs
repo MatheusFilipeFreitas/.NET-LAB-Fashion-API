9e05fcd57145726028803303008bbc1be1138d22
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-06-19 19:50:07 -0300

    DTO de Modelo

---
+++ LAB-Fashion-API/Dto/ModelDto/AddModelDto.cs
@@ -0,0 +1,17 @@
+﻿using LAB_Fashion_API.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace LAB_Fashion_API.Dto.ModelDto
+{
+    public class AddModelDto
+    {
+        [Required(ErrorMessage = "É requerido o Nome!")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "É requerido o Identificador de Coleção!")]
+        public int CollectionId { get; set; }
+        [Required(ErrorMessage = "É requerido o Tipo!")]
+        public ModelType Type { get; set; }
+        [Required(ErrorMessage = "É requerido o Layout!")]
+        public LayoutType Layout { get; set; }
+    }
+}
