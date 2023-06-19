59be32471bec12743eebad446a504a8b37a6c5b1
MatheusFilipeFreitas <mathffreitas@hotmail.com>
2023-06-19 20:30:03 -0300

    Model de Colecao

---
+++ LAB-Fashion-API/Enums/Seasons.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace LAB_Fashion_API.Enums
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Seasons
+    {
+        Winter = 1,
+        Autumn = 2,
+        Spring = 3,
+        Summer = 4,
+    }
+}
---
+++ LAB-Fashion-API/Models/Collection.cs
@@ -0,0 +1,28 @@
+﻿using LAB_Fashion_API.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LAB_Fashion_API.Models
+{
+    [Index(nameof(Name), IsUnique = true)]
+    public class Collection
+    {
+        [Key]
+        public int Id { get; set; }
+        [NotNull]
+        public string Name { get; set; } = string.Empty;
+        [NotNull]
+        public int AccountableId { get; set; }
+        [NotNull]
+        public string Brand { get; set; } = string.Empty;
+        [NotNull]
+        public Double Budget { get; set; }
+        [NotNull]
+        public DateTime Release { get; set; }
+        [NotNull]
+        public Seasons Season { get; set; }
+        [NotNull]
+        public StatusType Status { get; set; }
+
+    }
+}
